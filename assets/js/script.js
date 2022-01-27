bdcfb50014df8e7c16acc599246bf1d04cf3b44e
kelcmitch97 <kelciemitchelmore@gmail.com>
2022-01-26 22:13:00 -0330

    Set and Get items from local storage

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,42 +1,50 @@
 // Current date on Jumbotron
 var currentDay = $("#currentDay")
-.text(moment().format('dddd, MMMM Do YYYY'));
+    .text(moment().format('dddd, MMMM Do YYYY'));
 $(".lead").append(currentDay);
 
-// Save textarea events to localStorage
-$(".saveBtn").on("click", function() {
-    // get textarea values
-    var eventText = $(".description").val();
+// Time block past, present and future checks
+var timeBlock = $(".row textarea[type=text]");
 
-   localStorage.setItem("eventText", JSON.stringify(eventText));
+$(timeBlock).each(function () {
+    var timeNow = moment().format("k");
+    var blockTime = parseInt($(this).attr("id"));
+
+    if (blockTime < timeNow) {
+        $(this).addClass("past");
+        // $(this).removeClass("present");
+        // $(this).removeClass("future");
+    } else if (blockTime === timeNow) {
+        $(this).addClass("present");
+        // $(this).removeClass("past");
+        // $(this).removeClass("future");
+    } else if (blockTime > timeNow) {
+        $(this).addClass("future");
+        // $(this).removeClass("present");
+        // $(this).removeClass("past");
+    }
 });
 
-// Time block past, present and future checks
-function timeTracker() {
-    var timeNow = moment().hour();
 
-    $(".time-block").each(function() {
-        var blockTime = parseInt($(this).attr("id").split("hour")[1]);
+// Save textarea events to localStorage
+$(".saveBtn").click(function () {
+    event.preventDefault();
 
-        if (blockTime < timeNow) {
-            $(this).addClass("past");
-        } else if (blockTime === timeNow) {
-            $(this).addClass("present"); 
-        } else {
-            $(this).addClass("future");
-        }
-    })
-};
+    // get textarea values
+    var eventText = $(this).attr("hour");
+    var eventTime = $(this).prev().val();
 
-// Get text from localStorage
-$("#hour1 .description").val(localStorage.getItem("hour1"));
-$("#hour2 .description").val(localStorage.getItem("hour2"));
-$("#hour3 .description").val(localStorage.getItem("hour3"));
-$("#hour4 .description").val(localStorage.getItem("hour4"));
-$("#hour5 .description").val(localStorage.getItem("hour5"));
-$("#hour6 .description").val(localStorage.getItem("hour6"));
-$("#hour7 .description").val(localStorage.getItem("hour7"));
-$("#hour8 .description").val(localStorage.getItem("hour8"));
-$("#hour9 .description").val(localStorage.getItem("hour9"));
+    localStorage.setItem(eventText, JSON.stringify(eventTime));
+});
 
-timeTracker();
+// Get items from localStorage
+
+$("#9").val(localStorage.getItem("9"));
+$("#10").val(localStorage.getItem("10"));
+$("#11").val(localStorage.getItem("11"));
+$("#12").val(localStorage.getItem("12"));
+$("#13").val(localStorage.getItem("13"));
+$("#14").val(localStorage.getItem("14"));
+$("#15").val(localStorage.getItem("15"));
+$("#16").val(localStorage.getItem("16"));
+$("#17").val(localStorage.getItem("17"));
